773e7cd04781f0aca64736d54f1547487d07e2c2
Beto Farías <beto@2gom.com.mx>
2019-01-25 22:59:08 -0600

    Primer comit
    Creación del juego
    Creación de enemigos etapa 1

---
+++ Assets/scripts/Enemy1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+public class Enemy1 : MonoBehaviour
+{
+
+    public GameObject target;
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //agent = GetComponent<NavMeshAgent>();
+
+        //Vector3 destination = target.transform.position;
+
+        //agent.SetDestination(destination); //(destination.x,destination.y,destination.z);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/NivelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NivelManager : MonoBehaviour
+{
+
+
+    public GameObject[] startPoints;
+    public GameObject[] enemies;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateEnemy();
+        CreateEnemy();
+        CreateEnemy();
+        CreateEnemy();
+        CreateEnemy();
+        CreateEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void CreateEnemy() {
+        Debug.Log("Creando enemigo .... ");
+
+        //Seleccion del punto de creacion
+        int ran = Random.Range(0, startPoints.Length);
+
+        Debug.Log("Start point " + ran);
+        Debug.Log("Start point lem " + startPoints.Length);
+        GameObject startPoint = startPoints[ran];
+
+        //Seleccion del enemigo
+        ran = Random.Range(0, enemies.Length);
+        GameObject enemy = enemies[ran];
+
+
+
+        //Creacion del enemigo
+        NavMeshAgent agent = (Instantiate(enemy, startPoint.transform.position, new Quaternion()) as GameObject).GetComponent<NavMeshAgent>();
+       
+        //Asigna el destino del agente creado
+        agent.SetDestination(target.transform.position);
+
+     }
+}
